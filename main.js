a80cc9c7b83841dbc85984cca08323f1a77701a9
Reagan DeLong <rdelong17@gmail.com>
2023-02-08 21:55:01 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+function checkvalue(val) {
+    if(val === "other")
+        document.getElementById("other").style.display="block";
+   else    
+        document.getElementById("other").style.display="none";
+}
